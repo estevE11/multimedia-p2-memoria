1ce429d7f5b4f92b8bf00319e6d96f553c5b605d
Roger Esteve <rogeresteve30@gmail.com>
2022-05-12 20:46:25 +0200

    diferents tamanys

--- js/p002-memoria.js
+++ js/p002-memoria.js
@@ -1,6 +1,25 @@
 var ampladaCarta, alcadaCarta;
 var separacioH=20, separacioV=20;
-var nFiles=2, nColumnes=2;
+var nFiles = 2, nColumnes = 2;
+
+var cards = [
+    'carta1',
+    'carta1',
+    'carta14',
+    'carta14',
+    'carta2',
+    'carta2',
+    'carta3',
+    'carta3',
+    'carta4',
+    'carta4',
+    'carta5',
+    'carta5',
+    'carta15',
+    'carta15',
+    'carta16',
+    'carta16',
+];
 
 var jocCartes = [
     'carta5', 'carta5',
@@ -9,31 +28,38 @@ var jocCartes = [
 
 var c0, c1;
 
-$(function(){
+$(function () {
     var f, c, carta;
- 
-    console.log(jocCartes);
-    jocCartes = suffleCards(jocCartes);
-    console.log(jocCartes);
 
-    ampladaCarta=$(".carta").width(); 
-    alcadaCarta=$(".carta").height();
+    if (nFiles == 2) {
+        cards = cards.splice(0, 4);
+    }
+    jocCartes = cards;
+    
+    jocCartes = suffleArray(jocCartes);
+
+    ampladaCarta = $(".carta").width();
+    alcadaCarta = $(".carta").height();
     // mida del tauler
     $("#tauler").css({
-        "width" : "220px",
+        "width": "220px",
         "height": "300px"
     });
     // inicialitzem totes les cartes: posició
-    for(f=1; f<=nFiles; f++)
-        for(c=1; c<=nColumnes; c++){
-            carta=$("#f"+f+"c"+c);
-            carta.css({
-                "left" :  ((c-1)*(ampladaCarta+separacioH)+separacioH)+"px",
-                "top"  :  ((f-1)*(alcadaCarta+separacioV) +separacioV)+"px"
-            });
-            carta.find(".davant").addClass(jocCartes.pop());
-           
+    for (f = 1; f <= nFiles; f++) {
+        for (c = 1; c <= nColumnes; c++) {
+            let top = ((f - 1) * (alcadaCarta + separacioV) + separacioV);
+            let left = ((c - 1) * (ampladaCarta + separacioH) + separacioH);
+            carta = $('<div class="carta" id="' + 'f' + f + 'c' + c + '"' + ' style="top:' + top + '; left: ' + left + '"> <div class="cara darrera"></div><div class="cara davant"></div></div>');
+            $('#tauler').append(carta);
         }
+    }
+    for (f = 1; f <= nFiles; f++) {
+        for (c = 1; c <= nColumnes; c++) {
+            carta = $("#f" + f + "c" + c);
+            carta.find(".davant").addClass(jocCartes.pop());
+        }
+    }
 
     $(".carta").click(function () {
         $(this).toggleClass("carta-girada");
@@ -60,16 +86,24 @@ $(function(){
 });
 
 
-function suffleCards(cards) { 
+/**
+ * Barrejar aleatoriament un array
+ * 
+ * @param {Array} arr
+ * @returns Array
+ */
+function suffleArray(arr) { 
     let res = [];
     
-    let len = cards.length;
+    let len = arr.length;
 
     for (let i = 0; i < len; i++) { 
-        let random_card_index = Math.floor(Math.random()*cards.length);
-        res.push(cards[random_card_index]);
-        cards.splice(random_card_index, 1);
-        console.log(cards);
+        let random_card_index = Math.floor(Math.random()*arr.length);
+        res.push(arr[random_card_index]);
+        arr.splice(random_card_index, 1);
+        console.log('----');
+        console.log(arr);
+        console.log(res);
     }
 
     return res;
